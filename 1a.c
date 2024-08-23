1abbf55bd2364edc7385c91d4e9f748a47483f45
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-08-23 19:13:50 +0530

    sym Link created

--- 1a.c
+++ 1a.c
@@ -1,15 +1,31 @@
+/*
+	Name : Himanshu Shivhare
+	Roll No. : MT2024058
+	Program :a. soft link (symlink system call)
+*/
+
 #include<stdio.h>
 #include<unistd.h>
 
-int main(){
-	const char*dest = "hello1.txt";
-	const char*src = "hello1_src.txt";
+// ls -l           -> used to show all the files in list format
 
-	if(symlink(dest, src) != 0){
-		perror("symlink");
+
+int main(){
+	printf("Hello Program /n");
+	const char*dest = "hello1.txt";
+	const char*src = "hello2_src.txt";
+	int res = symlink(dest , src);
+	if(res != 0){
+		perror("symlink____________");  // if file is already been created then it will show the error.
 		return 1;
+	
+	}else{
+		printf("Soft Link is Created succesfully! ");
 	
 	}
 
-printf("The Link is been Created");
+
 }
+
+//     ./a.out
+//     symlink____________: File exists
