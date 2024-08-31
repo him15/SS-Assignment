b251ce7577615064063d6b9736b2b0be62c22a46
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-08-31 23:09:06 +0530

    program 18 b

---
+++ 18b.c
@@ -0,0 +1,47 @@
+/*
+Himanshu Shivhare
+MT2024058
+*/
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main()
+{
+    int fd = open("18txt.txt", O_RDONLY);
+    struct flock rdlock = {F_RDLCK, SEEK_SET, 0, 34, getpid()};
+    int id;
+    printf("Enter record id:");
+    scanf("%d", &id);
+    switch (id)
+    {
+    case 1:
+        rdlock.l_start = 0;
+        break;
+    case 2:
+        rdlock.l_start = 17;
+        break;
+    case 3:
+        rdlock.l_start = 34;
+        break;
+    default:
+        perror("invalid choise of record!!!");
+        return 0;
+    }
+    fcntl(fd, F_SETLKW, &rdlock);
+    printf("record %d id locked Enter to unlock it\n", id);
+    getchar();
+    getchar();
+    rdlock.l_type = F_ULOCK;
+    fcntl(fd, F_SETLKW, &rdlock);
+    printf("record is unlocked\n");
+    return 0;
+}
+/*
+Enter record id:1
+record 1 id locked Enter to unlock it
+
+record is unlocked
+*/
