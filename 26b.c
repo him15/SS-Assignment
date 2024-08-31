4df72ccacff1da272f13d2cb7cc5887a18495173
Him15 <Himanshu.Shivhare@iiitb.ac.in>
2024-08-31 23:42:26 +0530

    Program 26b

---
+++ 26b.c
@@ -0,0 +1,18 @@
+/*
+Himanshu Shivhare
+MT2024058
+
+/*
+Write a program to execute an executable program.
+b. pass some input to an executable program. (for example execute an executable of $./a.out name)
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+    char *path=argv[1];
+    argv++;
+    execv(path, argv);
+    return 0;
+}
